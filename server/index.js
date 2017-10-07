6f2eb78aca80d4937fa18115765323bc135ad43a
Mike Fleming <flemingatx@gmail.com>
2017-10-07 14:18:50 -0700

    basic hello world

--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
 'use strict';
 
+const http = require('http');
+const router = require('find-my-way')();
+
 exports.listen = (port) => {
-  // insert backend here
+  router.on('GET', '/', (req, res, params) => {
+    res.end('{ "message": "hello world! 🎃" }\n');
+  });
+
+  const server = http.createServer((req, res) => {
+    router.lookup(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+  });
 };
