74ee6a1e613dc6ca542ed9a038ce15e646b29d5e
Mike Fleming <flemingatx@gmail.com>
2017-10-07 12:01:51 -0700

    builds basic frontend

--- client/index.js
+++ client/index.js
@@ -1,1 +1,32 @@
-// insert frontend here
+'use strict';
+
+const html = require('choo/html');
+const choo = require('choo');
+
+const app = choo();
+
+app.use(titleStore);
+app.route('/', mainView);
+app.mount('body');
+
+function mainView (state, emit) {
+  return html`<body>
+                  <h1>${state.title}</h1>
+                  <input type="text" value="${state.title}" oninput=""${update} />
+              </body>`;
+}
+
+function update (e) {
+  emit('update', e.target.value);
+}
+
+function titleStore(state, emitter) {
+  state.title = 'Not quite set yet';
+  emitter.on('DOMContentLoaded', function () {
+    emitter.on('update', function (newTitle) {
+      state.title = newTitle;
+      emitter.emit('render');
+    });
+  });
+}
+
