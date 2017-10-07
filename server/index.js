93050a0b06caad46d5888a63afa49b8a0d9a147d
Mike Fleming <flemingatx@gmail.com>
2017-10-06 20:34:18 -0700

    completes basic

--- server/index.js
+++ server/index.js
@@ -1,4 +1,29 @@
 'use strict';
 
-const fastify = require('fastify');
+const fastify = require('fastify')();
 
+exports.listen = (port) => {
+  const schema = {
+    schema: {
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            hello: {
+              type: 'string'
+            }
+          }
+        }
+      }
+    }
+  };
+
+  fastify.get('/', schema, (req, reply) => {
+    reply.send({ hello: 'world' });
+  });
+
+  fastify.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`server listening on ${fastify.server.address().port}`);
+  });
+};
