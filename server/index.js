25204f404d01daf10b3ca405a0d69cfc1539e8b3
Mike Fleming <flemingatx@gmail.com>
2017-10-07 12:07:35 -0700

    adds backend

--- server/index.js
+++ server/index.js
@@ -1,5 +1,23 @@
 'use strict';
 
-exports.listen = (port) => {
-  // insert backend here
+// create a new app
+const fastify = require('fastify')();
+
+exports.listen = function (port) {
+
+  const options = {
+    something: true,
+  };
+
+  // register multiple routes (fastify uses them like middleware)
+  fastify.register([
+    require('./route'),
+  ], options, function (err) {
+    if (err) throw err;
+  });
+
+  fastify.listen(port, function (err) {
+    if (err) throw err;
+    console.log(`fastify listening on ${fastify.server.address().port}`);
+  });
 };
