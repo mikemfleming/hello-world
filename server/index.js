1a5f0c4ea5d1b038f8607d70dc12cb9816c0ce5a
Mike Fleming <flemingatx@gmail.com>
2017-10-06 21:48:53 -0700

    adds comments

--- server/index.js
+++ server/index.js
@@ -3,9 +3,13 @@
 const Hapi = require('hapi');
 
 exports.listen = (port) => {
+  // create a new hapi server object
   const server = new Hapi.Server();
+
+  // add a connection to the server
   server.connection({ port: port, host: 'localhost' });
 
+  // start the server
   server.start((err) => {
     if (err) throw err;
     console.log(`hapi is listening on: ${server.info.uri}`);
