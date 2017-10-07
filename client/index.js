4baefbb1328b16f547622069414f103ffaee4f89
Mike Fleming <flemingatx@gmail.com>
2017-10-07 15:36:44 -0700

    adds counter

--- client/index.js
+++ client/index.js
@@ -6,6 +6,7 @@ const choo = require('choo');
 const app = choo();
 
 app.use(titleStore);
+app.use(countStore);
 app.route('/', mainView);
 app.mount('body');
 
@@ -14,15 +15,31 @@ function mainView (state, emit) {
             <body>
                 <h1>${state.title}</h1>
                 <input type="text" value="${state.title}" oninput=${update} />
+                <h1>count is ${state.count}</h1>
+                 <button onclick=${increment}>Increment</button>
             </body>
         `;
 
   function update (e) {
     emit('update', e.target.value);
   }
+
+  function increment (e) {
+    emit('increment', e.target.value);
+  }
 }
 
-function titleStore(state, emitter) {
+function countStore (state, emitter) {
+  state.count = 0;
+  emitter.on('DOMContentLoaded', function () {
+    emitter.on('increment', function () {
+      state.count = ++state.count;
+      emitter.emit('render');
+    });
+  });
+}
+
+function titleStore (state, emitter) {
   state.title = 'Hello new favorite frameworks! o.O';
   emitter.on('DOMContentLoaded', function () {
     emitter.on('update', function (newTitle) {
