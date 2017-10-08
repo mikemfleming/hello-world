e626204d60dcc0881ec44ab2a8d451d9c4243409
Mike Fleming <flemingatx@gmail.com>
2017-10-07 20:02:55 -0500

    basic node server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 'use strict';
 
-exports.listen = (port) => {
-  // insert backend here
+const http = require('http');
+
+exports.listen = function (port) {
+  const server = http.createServer(function (req, res) {
+    console.log(typeof res)
+    res.end('{ "hello": "world" }\n');
+  });
+
+  server.listen(port, function () {
+    console.log(`node is listening on ${port}`);
+  });
 };
