8bbb97b6c0d77f29d970f05ef26550742f0aecf0
Mike Fleming <flemingatx@gmail.com>
2017-10-10 11:29:47 -0500

    handles POST

--- server/index.js
+++ server/index.js
@@ -10,14 +10,23 @@ exports.listen = (port) => {
   server.connection({ port: port, host: 'localhost' });
 
   // add routes
-  server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-      // encodeURIComponent prevents content injection attacks
-      reply(`Hello, ${encodeURIComponent(request.params.name)}!\n`);
+  server.route([
+    {
+      method: 'GET',
+      path: '/{name}',
+      handler: function (request, reply) {
+        // encodeURIComponent prevents content injection attacks
+        reply(`Hello, ${encodeURIComponent(request.params.name)}!\n`);
+      }
+    },
+    {
+      method: 'POST',
+      path: '/',
+      handler: function (request, reply) {
+        reply(`${request.payload}\n`);
+      }
     }
-  });
+  ]);
 
   // start the server
   server.start((err) => {
