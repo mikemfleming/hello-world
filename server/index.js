19cbb4d20cf7411ad164d0c951b1db3993244544
miek <flemingatx@gmail.com>
2017-10-10 21:46:46 -0500

    response with headers method url and body

--- server/index.js
+++ server/index.js
@@ -3,8 +3,32 @@
 const http = require('http');
 
 exports.listen = function (port) {
-  const server = http.createServer(function (req, res) {
-    res.end('{ "hello": "world" }\n');
+
+  // create a new EventEmitter object
+  const server = http.createServer();
+
+  // attach a req listener
+  server.on('request', function (request, response) {
+    const { headers, method, url } = request;
+    let body = [];
+    // request has a stream on it we can listen to
+    request.on('error', function (error) {
+      console.log(error);
+    }).on('data', function (chunk) {
+      body.push(chunk);
+    }).on('end', function () {
+      body = Buffer.concat(body).toString();
+
+      response.on('error', function (error) {
+        console.log(error);
+      });
+
+      response.writeHead(200, { 'content-type': 'application/json'});
+
+      const responseBody = { headers, method, url, body };
+
+      response.end(JSON.stringify(responseBody, null, 4) + '\n');
+    });
   });
 
   server.listen(port, function () {
